9f53040f0320e87ba2bf96ba02d1ce51ac414b2a
EC2 Default User <ec2-user@ip-172-31-7-27.ec2.internal>
2020-11-23 07:26:27 +0000

    [Add]画像投稿機能

--- sample_app_practice/app/controllers/todolists_controller.rb
+++ sample_app_practice/app/controllers/todolists_controller.rb
@@ -1,4 +1,5 @@
 class TodolistsController < ApplicationController
+
   def new
     @list = List.new
   end
@@ -29,7 +30,7 @@ class TodolistsController < ApplicationController
 
 private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 
 end
--- sample_app_practice/config/initializers/application_controller_renderer.rb
+++ sample_app_practice/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '07b96a23aec60ebe1ec946dade88607c87c5edcd9643f568edd0e44388571b00f5f594b009c34db579bf29bef74c7de3ef08f441553065b36f17daec28e2d894'
